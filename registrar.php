334f2694a33e230261df96fafa9ea353f6e6e380
Alfonso <abengar577a@ieslacampina.es>
2023-02-23 17:03:45 +0100

    Creo todo el formulario con sus correspondientes variables

--- registrar.php
+++ registrar.php
@@ -15,3 +15,42 @@
 			<div class="row">
 				<h1>Socios</h1>
 			</div>
+            <div class="row">
+				<div class="col-md-8">
+					<!-- Completar atributos de form -->
+					<form action="registrar2.php" id="registro" name="registro" autocomplete="off" method="post">
+						<div class="form-group">
+							<!-- Nombre -->
+							<label for="nombre">Nombre</label>
+							<input type="text" name="nombre" id="nombre" class="form-control" placeholder="Introduce el nombre" required>
+						</div>
+						
+						<div class="form-group">
+							<!-- Teléfono -->
+							<label for="telefono">Teléfono</label>
+							<input type="tel" name="telefono" id="telefono" class="form-control" placeholder="Introduce el teléfono" required>
+						</div>
+						
+						<div class="form-group">
+							<!-- Fecha nacimiento -->
+							<label for="fecha">Fecha de nacimiento</label>
+							<input type="date" name="fecha" id="fecha" class="form-control" required>
+						</div>
+						
+						<div class="form-group">
+							<!-- Categoría -->
+							<label for="categoria">Categoría</label>
+							<select name="categoria" id="categoria" class="form-control" required>
+								<option value="AMATEUR">AMATEUR</option>
+								<option value="PROFESIONAL">PROFESIONAL</option>
+							</select>
+						</div>
+						
+						<div class="form-group">
+							<!-- Registrar -->
+							<input type="submit" value="Registrar" class="btn btn-primary">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
