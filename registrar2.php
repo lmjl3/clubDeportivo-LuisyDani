e6324199866f2db61d1c0c6322f888d52c325b9c
Alfonso <abengar577a@ieslacampina.es>
2023-02-23 17:07:41 +0100

    Añado estructura principal y toda la funcionalidad

--- registrar2.php
+++ registrar2.php
@@ -10,3 +10,37 @@
 		
 		<title>Club Deportivo La Venta</title>
 	</head>
+    <body>
+		<?php
+			$nombre = $_POST['nombre'];
+			$telefono = $_POST['telefono'];
+			$fecha = $_POST['fecha'];
+			$categoria = $_POST['categoria'];
+			
+			//Establezco conexión
+			require 'conexion.php';
+
+			//Preparo la sentencia SQL
+			$sql = "INSERT INTO clubdeportivo (nombre,telefono,fecha_nacimiento,categoria) VALUES ('$nombre','$telefono','$fecha','$categoria')";
+
+			//Ejecuto la sentencia y guardo el resultado
+			$resultado = $mysqli->query($sql);
+
+			if($resultado>0){
+		?>
+				<br>
+				<p class="alert alert-primary">REGISTRO AGREGADO</p>
+  					
+
+		<?php
+			} else {
+		?>
+				<br>
+  				<p class="alert alert-danger">REGISTRO NO AGREGADO</p>
+		<?php		
+			}
+		?>
+			<br>
+			<p><a href="index.php" class="btn btn-primary">Regresar</a></p>
+	</body>
+</html>
