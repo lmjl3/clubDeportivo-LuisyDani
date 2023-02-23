7ea177bb8e0fd2e67d2133c13fc55157b42616ca
daniel <dgamgar755a@ieslacampina.es>
2023-02-23 17:08:27 +0100

    Añado los condicionales

--- editar2.php
+++ editar2.php
@@ -22,6 +22,19 @@
 
 			//Ejecutamos sentencia y guardamos resultado
 			$resultado = $mysqli->query($sql);
+            if($resultado>0){
+                ?>
+                        <br>
+                        <p class="alert alert-primary">REGISTRO MODIFICADO</p>
+                <?php
+                    } else {
+                ?>
+                        <br>
+                          <p class="alert alert-danger">REGISTRO NO MODIFICADO</p>
+                <?php
+                    }
+                ?>
+                    <br>
     ?>
 </body>
 </html>
