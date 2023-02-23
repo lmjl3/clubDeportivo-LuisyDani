6a2b2175f52a8662a6b45311a969752cd37fe3b3
daniel <dgamgar755a@ieslacampina.es>
2023-02-23 17:02:26 +0100

    Añado formulario

--- editar.php
+++ editar.php
@@ -28,7 +28,40 @@
         </div>
         <div class="row">
             <div class="col-md-8">
-                
+            <form id="registro" name="registro" autocomplete="off" action="editar2.php" method="post">
+						<!-- Incluir el id en algún div de estos-->
+						<div class="form-group">
+							<!-- Nombre  -->
+							<input type="hidden" name="id" value="<?php echo $id; ?>">
+							<label for="nombre">Nombre</label>
+							<input type="text" name="nombre" id="nombre" class="form-control" placeholder="Introduce el nombre" value="<?php echo $fila['nombre']; ?>" required>
+						</div>
+
+						<div class="form-group">
+							<!-- Teléfono -->
+							<label for="telefono">Teléfono</label>
+							<input type="tel" name="telefono" id="telefono" class="form-control" placeholder="Introduce el teléfono" value="<?php echo $fila['telefono']; ?>" required>
+						</div>
+
+						<div class="form-group">
+							<!-- Fecha nacimiento -->
+							<label for="fecha">Fecha de nacimiento</label>
+							<input type="date" name="fecha" id="fecha" class="form-control" value="<?php echo $fila['fecha_nacimiento']; ?>" required>
+						</div>
+						
+						<div class="form-group">
+							<!-- Categoría -->
+							<label for="categoria">Categoría</label>
+							<select name="categoria" id="categoria" class="form-control" required>
+								<option value="AMATEUR" <?php if($fila['categoria'] == 'AMATEUR'){echo "selected";} ?>>AMATEUR</option>
+								<option value="PROFESIONAL" <?php if($fila['categoria'] == 'PROFESIONAL'){echo "selected";} ?>>PROFESIONAL</option>
+							</select>
+						</div>
+						
+						<div class="form-group">
+							<!-- Editar -->
+							<input type="submit" value="Editar" class="btn btn-primary">
+						</div>
             </div>
         </div>
     </div>
