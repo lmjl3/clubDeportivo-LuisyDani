34a7ed1a23e8fcd18bc98dfc527aedcb7c05c471
Alfonso <abengar577a@ieslacampina.es>
2023-02-23 17:04:49 +0100

    añado estructura final y cierro el html

--- registrar.php
+++ registrar.php
@@ -54,3 +54,9 @@
 				</div>
 			</div>
 		</div>
+        <!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="js/jquery-3.4.1.min.js" ></script>
+		<script src="js/bootstrap.min.js" ></script>
+	</body>
+</html>	
